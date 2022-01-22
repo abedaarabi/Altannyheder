ad46384e057ade6db913fe68f3bf1d87e619bd2b
Abdulrahman Mohamad Aarabi <abma@moe.dk>
2022-01-22 23:01:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Altannyheder</h1>
+      <div className="container">
+        <div className="text">
+          <h2>
+            Kære alle. Jeg har lavet denne hjemmeside, så jeg kan holde jer
+            opdaterede omkring altanprojektet. Jeg vil løbende poste
+            information/updates fra Altan.dk, Boligexperten mv.
+          </h2>
+
+          <p>
+            <p className="dato">Dato: 20 Jan 2022</p>
+            Altan.dk: "Første step i processen er, at jeg skal udarbejde
+            materialet til ansøgningen om byggetilladelsen, herunder skal jeres
+            ejendom have foretagen opmåling af en af mine kollegaer og tegninger
+            hentes på kommunen. Jeg forventer, at kunne sende jeres projekt
+            afsted til kommunen senest i uge 10. <br></br>
+            Efter jeg har indsendt projektet, vil jeg være i løbende dialog med
+            kommunen og jeg skal selvfølgelig nok informere jer løbende om
+            projektets status. Så snart jeg modtager byggetilladelsen, vil jeg
+            selvfølgelig give jer information herom. Jeg håber, at dette giver
+            jer et overblik over, hvad der kommer til at ske den næste periode."
+          </p>
+        </div>
+        <div>
+          <img
+            src="https://media2.giphy.com/media/3b7NSZfIHA0TrXdJCu/giphy-downsized-large.gif"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 }
